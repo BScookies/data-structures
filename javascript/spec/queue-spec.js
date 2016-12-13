9cc6d5b54f73eabeae962f4739759c7549fc2e38
Forrest Labrum <forrest.labrum@gmail.com>
2016-12-12 19:18:16 -0700

    refactor spec to choose type of queue from an array

--- javascript/spec/queue-spec.js
+++ javascript/spec/queue-spec.js
@@ -1,49 +1,51 @@
 'use strict';
 const expect = require('chai').expect;
-const QueueArray = require('../src/queue.js').QueueArray;
+const Queue = require('../src/queue.js');
 
-describe('QueueArray', function() {
-  let queue;
-  beforeEach(function() {
-    queue = new QueueArray;
-  });
+['QueueArray', 'QueueLinkedList'].forEach(className => {
+  describe(className, function() {
+    let queue;
+    beforeEach(function() {
+      queue = new Queue[className];
+    });
 
-  it('Should create a queue', function() {
-    expect(queue).to.not.be.undefined;
-  });
+    it('Should create a queue', function() {
+      expect(queue).to.not.be.undefined;
+    });
 
-  it('Should have storage as an array', function() {
-    expect(Array.isArray(queue.storage)).to.equal(true);
-  });
+    it('Should have storage as an array', function() {
+      expect(Array.isArray(queue.storage)).to.equal(true);
+    });
 
-  it('Should add items to the end and remove from the front', function() {
-    queue.enqueue(4);
-    queue.enqueue(13);
-    queue.enqueue({ test: 'test' });
-    queue.enqueue('wooooh');
+    it('Should add items to the end and remove from the front', function() {
+      queue.enqueue(4);
+      queue.enqueue(13);
+      queue.enqueue({ test: 'test' });
+      queue.enqueue('wooooh');
 
-    expect(queue.dequeue()).to.equal(4);
-    expect(queue.dequeue()).to.equal(13);
-  });
+      expect(queue.dequeue()).to.equal(4);
+      expect(queue.dequeue()).to.equal(13);
+    });
 
-  it('Should return correct size and report if empty', function() {
-    expect(queue.size()).to.equal(0);
-    expect(queue.isEmpty()).to.equal(true);
+    it('Should return correct size and report if empty', function() {
+      expect(queue.size()).to.equal(0);
+      expect(queue.isEmpty()).to.equal(true);
 
-    queue.enqueue('some stuff');
-    queue.enqueue('some more stuff');
+      queue.enqueue('some stuff');
+      queue.enqueue('some more stuff');
 
-    expect(queue.size()).to.equal(2);
-    expect(queue.isEmpty()).to.equal(false);
+      expect(queue.size()).to.equal(2);
+      expect(queue.isEmpty()).to.equal(false);
 
-    queue.dequeue();
+      queue.dequeue();
 
-    expect(queue.size()).to.equal(1);
-    expect(queue.isEmpty()).to.equal(false);
+      expect(queue.size()).to.equal(1);
+      expect(queue.isEmpty()).to.equal(false);
 
-    queue.dequeue();
+      queue.dequeue();
 
-    expect(queue.size()).to.equal(0);
-    expect(queue.isEmpty()).to.equal(true);
+      expect(queue.size()).to.equal(0);
+      expect(queue.isEmpty()).to.equal(true);
+    })
   })
-})
+});
