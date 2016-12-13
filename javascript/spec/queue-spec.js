ee438645c8816cf8cb98cbd42077636b76ac0dea
Forrest Labrum <forrest.labrum@gmail.com>
2016-12-12 19:07:35 -0700

    add enqueue and dequeue methods for simple array queue

--- javascript/spec/queue-spec.js
+++ javascript/spec/queue-spec.js
@@ -15,4 +15,14 @@ describe('QueueArray', function() {
   it('Should have storage as an array', function() {
     expect(Array.isArray(queue.storage)).to.equal(true);
   });
+
+  it('Should add items to the end and remove from the front', function() {
+    queue.enqueue(4);
+    queue.enqueue(13);
+    queue.enqueue({ test: 'test' });
+    queue.enqueue('wooooh');
+
+    expect(queue.dequeue()).to.equal(4);
+    expect(queue.dequeue()).to.equal(13);
+  });
 })
--- javascript/src/queue.js
+++ javascript/src/queue.js
@@ -2,4 +2,12 @@ const QueueArray = function() {
   this.storage = [];
 }
 
+QueueArray.prototype.enqueue = function(val) {
+  return this.storage.push(val);
+}
+
+QueueArray.prototype.dequeue = function(val) {
+  return this.storage.shift();
+}
+
 module.exports = { QueueArray };
