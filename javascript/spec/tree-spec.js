947a8d4f5f1957fd092a7ef9d988e1b9e9ae91a9
Forrest Labrum <forrest.labrum@gmail.com>
2016-12-13 21:59:22 -0700

    add addChild method to Node class

--- javascript/spec/tree-spec.js
+++ javascript/spec/tree-spec.js
@@ -1,7 +1,28 @@
 'use strict';
-const { Tree } = require('../src/tree.js');
+const { Tree, Node } = require('../src/tree.js');
 const { expect } = require('chai');
 
+describe('Node', function() {
+	let node = new Node(3);
+
+	it('Should create a node', function() {
+		expect(node).to.not.be.undefined;
+	});
+
+	it('Should have a value', function() {
+		expect(node.value).to.equal(3);
+	})
+
+	it('Should add a child', function() {
+		const child1 = node.addChild('hello');
+		node.addChild(14);
+		child1.addChild('One.. last trip.. to flavortown');
+
+		expect(node.children[0].value).to.equal('hello');
+		expect(node.children[1].value).to.equal(14);
+		expect(child1.children[0].value).to.equal('One.. last trip.. to flavortown');
+	});
+})
 describe('Tree', function() {
 	let tree;
 
